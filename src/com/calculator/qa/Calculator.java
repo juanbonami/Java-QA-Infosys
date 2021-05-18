2f9477e18fe70e45380bd27e2cda6ad5e2271d11
juan <juanbonami@gmail.com>
2021-05-17 19:27:20 -0500

    added division method in division branch

--- src/com/calculator/qa/Calculator.java
+++ src/com/calculator/qa/Calculator.java
@@ -1,10 +1,14 @@
 package com.calculator.qa;
 
 public class Calculator {
+	
+	public static double division(double num1, double num2) {
+		return num1 / num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(division(7,25));
 	}
 
 }
